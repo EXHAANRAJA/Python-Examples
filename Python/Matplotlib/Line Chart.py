8ea6532a5c2893859d4cd263ff0a8b9ddf74e3a1
EHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2021-03-26 15:10:21 +0500

    Color , Marker & Line Style Added

--- Python/Matplotlib/Line Chart.py
+++ Python/Matplotlib/Line Chart.py
@@ -7,8 +7,10 @@ year1 = [2010, 2012, 2014, 2016, 2018, 2020, 2021]
 pop1 = [197.4, 204.3, 209.3, 209.6, 208.73, 215.57, 218.48]
 
 # Build a Line Chart
-plt.plot(year, pop, 'k--', alpha=1, label="pakistan poluation")
-plt.plot(year1, pop1, 'b-', alpha=1, label="Indian poluation")
+plt.plot(year, pop, color='k', linestyle='--',
+         alpha=1, marker='.', label="pakistan poluation")
+plt.plot(year1, pop1, color='b', linestyle='-',
+         alpha=1, marker='o', label="Indian poluation")
 
 # Setting Label in x & y cordinate
 plt.xlabel("Year Wise Data")
