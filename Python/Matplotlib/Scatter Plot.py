dc9fd7e22e4710d62c6c13b126f534303d2ae8f1
EHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2021-03-26 14:27:32 +0500

    change of marker in scatter plot

--- Python/Matplotlib/Scatter Plot.py
+++ Python/Matplotlib/Scatter Plot.py
@@ -6,7 +6,7 @@ year = [2010, 2012, 2014, 2016, 2018, 2020, 2021]
 pop = [179.4, 187.3, 195.3, 203.6, 204.73, 208.57, 212.48]
 
 # Build a Line Chart
-plt.scatter(x=year, y=pop, color='green', s=10)
+plt.scatter(x=year, y=pop, color='green', s=10, marker="*")
 plt.grid()
 
 # Setting Label in x & y cordinate
