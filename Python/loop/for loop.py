67ab940e5a36edfda8531826b7f16cecb5702d94
AHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2021-03-28 14:48:15 +0500

    Looping & Iterations

---
+++ Python/loop/for loop.py
@@ -0,0 +1,18 @@
+# Addition
+# Program to find the sum of all numbers stored in a list
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+sum = 0
+
+for i in numbers:
+    sum = sum + i
+
+print("Total Sum is :" + str(sum))    
+
+# Create a list with range from start and end index
+
+#Multiply the number list
+multiply = 1
+for i in range(1,9):
+    multiply = multiply * i
+
+print("The Multiplication anwser is : " + str(multiply))    
